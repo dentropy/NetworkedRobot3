bfaccf2d600cb4350b8353660021f12196ec5503
Paul <paul@MSI-Ubuntu-17.10>
2018-03-01 10:59:48 -0500

    Modified the server code to also have functions to turn on and off an LED and started implementing serial code

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import serial
 app = Flask(__name__)
-
+ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 @app.route('/')
 def hello_world():
    return 'Hello World'
@@ -24,6 +25,16 @@ def right():
 @app.route('/stop')
 def stop():
    return 'stop'
+
+@app.route('/led_on')
+def led_on():
+   ser.write('O')
+   return 'led_on'
+
+@app.route('/led_off')
+def led_off():
+   ser.write('P')
+   return 'led_off'
 
 if __name__ == '__main__':
    app.debug = True
