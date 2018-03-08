f76d78f5edebb8c1a25cd76b2899b29ddedf42f0
Paul Mullins <noobie375@live.com>
2018-03-08 13:13:36 -0500

    In middle of adding ability to choose port via HTTP Now has functions to detect serial ports Server now uses templates to render a super basic uner interface for the robot

---
+++ server/ChooseSerialPort.py
@@ -0,0 +1,17 @@
+import serial
+import time
+from DetectSerialPort import serial_ports
+def choose_port():
+    #Find what serial porn the arduino is on
+    list_serial_ports = serial_ports()
+    print(type(list_serial_ports))
+    print(list_serial_ports)
+    if len(list_serial_ports) == 1:
+        return list_serial_ports[0]
+    else:
+        print("There is more than one available serialport from the following serial ports")
+        for i in range(len(list_serial_ports)):
+            print('Choice [' + str(i) + '] is called ' + str(list_serial_ports[i]))
+        serial_port_choice = int(input('Please make your choice : '))
+        print('You chose port ' + str(list_serial_ports[serial_port_choice]))
+        return list_serial_ports[serial_port_choice]
--- server/server.py
+++ server/server.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 import serial
+import time
+from ChooseSerialPort import choose_port
+from DetectSerialPort import serial_ports
+import socket
+'''
+#Find what serial porn the arduino is on
+list_serial_ports = serial_ports()
+print(type(list_serial_ports))
+print(list_serial_ports)
+if len(list_serial_ports) == 1:
+    tmp_port = list_serial_ports[0]
+    ser = serial.Serial(tmp_port, 9600, timeout=1)
+else:
+    print("There is more than one available serialport from the following serial ports")
+    for i in range(len(list_serial_ports)):
+        print('Choice [' + str(i) + '] is called ' + str(list_serial_ports[i]))
+    serial_port_choice = int(input('Please make your choice : '))
+    print('You chose port ' + str(list_serial_ports[serial_port_choice]))
+    ser = serial.Serial(list_serial_ports[serial_port_choice], 9600, timeout=1)
+'''
+#Get the ip address of the device this is running
+ip = socket.gethostbyname(socket.gethostname())
+#set defult direction and speed of robot of the robot
+direction = 'stop'
+speed = 0
+#The flask application
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+    return render_template('index.html', ip = ip, direction = direction)
 
+@app.route('/get_serial_ports/')
+def set_serial_port():
+    return str(serial_ports())
 @app.route('/forward')
 def forward():
    return 'forward'
@@ -37,5 +65,5 @@ def led_off():
    return 'led_off'
 
 if __name__ == '__main__':
-   app.debug = True
-   app.run()
+    app.debug = True
+    app.run()
